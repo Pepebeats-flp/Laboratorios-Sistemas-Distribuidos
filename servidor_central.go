d53a54eef634829c2fac681c52809dbcd94152e4
Pepe <josepintomuo@gmail.com>
2024-04-04 17:24:43 -0300

    mensajes

--- servidor_central.go
+++ servidor_central.go
@@ -25,16 +25,26 @@ func planet_asignation(n int) map[string]int {
 	return diccionario
 }
 
+// Función que retorna el planeta el nombre del planeta con menos botín
 func less_loot(planets map[string]int, n int) string {
-	min := n + 1
-	minName := ""
-	for key, v := range planets {
-		if v < min {
-			min = v
-			minName = key
+	// Definir el valor máximo
+	min := n
+
+	// Definir el planeta con menos botín
+	planeta_min := ""
+
+	// Recorrer el mapa
+	for planeta, botin := range planets {
+		// Comprobar si el botín es menor que el mínimo
+		if botin < min {
+			// Actualizar el mínimo
+			min = botin
+			// Actualizar el planeta con menos botín
+			planeta_min = planeta
 		}
 	}
-	return minName
+	fmt.Println("Planeta con menos botín:", planeta_min)
+	return planeta_min
 }
 
 func main() {
@@ -65,29 +75,40 @@ func main() {
 
 	// Planets loot
 	planets := planet_asignation(n)
-	fmt.Println(planets)
 
 	// Bucle infinito que escucha los mensajes, imprime el mensaje recibido y responden
 	for {
 
 		// Lee el mensaje
 		n, addr, err := conexion.ReadFromUDP(buffer_entrada)
+
 		// Imprime el mensaje
-		fmt.Println("->", string(buffer_entrada[0:n]), "desde", addr)
+		fmt.Println("Recepción de solicitud desde el Planeta", string(buffer_entrada[1:3]), ", del capitán", string(buffer_entrada[0:1]))
+
 		// Comprueba si el mensaje es STOP
 		if strings.TrimSpace(string(buffer_entrada[0:n])) == "STOP" {
 			fmt.Println("El servidor se ha detenido")
 			return
 		}
 
-		// Responde al mensaje
+		// Imprimimos estado actual de los planetas
+		fmt.Println("Estado actual de los planetas:" + fmt.Sprint(planets))
+
+		// Obtiene el planeta con menos botín
+		planeta_min := less_loot(planets, n)
+		// Le sumamos 1 al botín del planeta_min
+		planets[planeta_min] = planets[planeta_min] + 1
+		// Imprime el planeta asignado (Bot ́ın asignado al planeta PC, cantidad actual: 5)
+		fmt.Println("Botín asignado al planeta", planeta_min, ", cantidad actual:", planets[planeta_min])
+
+		// Responde al mensaje (no se si lo ocupemos xd)
 		mensaje := []byte("Mensaje recibido")
 		// Imprime el mensaje enviado
-		fmt.Println("Mensaje enviado ->", mensaje)
+		//fmt.Println("Mensaje enviado ->", mensaje)
 		// Envia el mensaje
 		_, err = conexion.WriteToUDP(mensaje, addr)
 		if err != nil {
-			fmt.Println(err)
+			//fmt.Println(err)
 			return
 		}
 	}
--- usuario.go
+++ usuario.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net"
 )
+
+func random_integer(n int) int {
+	return rand.Intn(n)
+}
+
+// Funcion que retorne un planeta al azar
+func planet_asignation(n int) string {
+	// Definir las claves "PA", "PB", ..., "PF"
+	claves := []string{"PA", "PB", "PC", "PD", "PE", "PF"}
+	// Elegir un planeta al azar y retornarlo
+	planeta := claves[random_integer(len(claves))]
+	return planeta
+}
+
+// Función que envia un mensaje al servidor central
+func send_message_to_server(capitan string, serverAddr string) {
+	// Establecer conexión con el servidor
+	conn, err := net.Dial("udp", serverAddr)
+	if err != nil {
+		fmt.Println("Error al conectar con el servidor:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Crear mensaje
+
+	message := []byte(capitan + planet_asignation(100))
+
+	// Enviar mensaje al servidor
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		fmt.Println("Error al enviar mensaje al servidor:", err)
+		return
+	}
+	fmt.Println("Capitán", capitan, "encontró botín en Planeta", string(message[1:]), ", enviando solicitud de asignación")
+}
 
 func main() {
 	// Dirección del servidor
@@ -17,15 +54,8 @@ func main() {
 	}
 	defer conn.Close()
 
-	// Mensaje a enviar al servidor
-	message := []byte("Mensaje de prueba")
-
 	// Enviar mensaje al servidor
-	_, err = conn.Write(message)
-	if err != nil {
-		fmt.Println("Error al enviar mensaje al servidor:", err)
-		return
-	}
-
-	fmt.Println("Mensaje enviado al servidor:", string(message))
+	send_message_to_server("1", serverAddr)
+	send_message_to_server("2", serverAddr)
+	send_message_to_server("3", serverAddr)
 }
