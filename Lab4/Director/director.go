5e7e25add3ed8d3762bd31ba790457ee55d808a5
Pepe <josepintomuo@gmail.com>
2024-05-14 22:48:00 -0400

    aaa

--- Lab4/Director/director.go
+++ Lab4/Director/director.go
@@ -17,24 +17,12 @@ func failOnError(err error, msg string) {
 func main() {
 	conn, err := amqp.Dial("amqp://dist:dist@10.35.169.51:5672/")
 
-	if err != nil {
-		log.Printf(" [x] Failed to connect to RabbitMQ")
-	} else {
-		log.Printf(" [x] Connected to RabbitMQ")
-	}
-
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
-
-	if err != nil {
-		log.Printf(" [x] Failed to open a channel")
-	} else {
-		log.Printf(" [x] Opened a channel")
-	}
 
 	q, err := ch.QueueDeclare(
 		"dosh_bank", // name
@@ -46,20 +34,8 @@ func main() {
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	if err != nil {
-		log.Printf(" [x] Failed to declare a queue")
-	} else {
-		log.Printf(" [x] Declared a queue")
-	}
-
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
-	if err != nil {
-		log.Printf(" [x] Failed to create a context")
-	} else {
-		log.Printf(" [x] Created a context")
-	}
 
 	body := "Mercenario_1,Piso_1"
 
