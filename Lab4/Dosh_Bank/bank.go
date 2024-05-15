7745eb393a5de15ada691b9c68b79849dfd93f45
Pepe <josepintomuo@gmail.com>
2024-05-15 16:59:54 -0400

    aaaaaa

--- Lab4/Dosh_Bank/bank.go
+++ Lab4/Dosh_Bank/bank.go
@@ -73,7 +73,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://dist:dist@dist042.inf.santiago.usm.cl:5672/")
+	conn, err := amqp.Dial("amqp://dist:dist@dist041.inf.santiago.usm.cl:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
