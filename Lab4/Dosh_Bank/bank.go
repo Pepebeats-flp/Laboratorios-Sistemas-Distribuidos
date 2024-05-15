a9e78dbd61ed784bf9255c344ac4bc1aabc965e3
Pepe <josepintomuo@gmail.com>
2024-05-15 16:12:51 -0400

    aa

--- Lab4/Dosh_Bank/bank.go
+++ Lab4/Dosh_Bank/bank.go
@@ -87,9 +87,7 @@ func main() {
 		nil,    // args
 	)
 	failOnError(err, "Failed to register a consumer")
-	if err != nil {
-		log.Fatalf("Failed to register a consumer: %s", err)
-	}
+
 	var forever chan struct{}
 
 	// Create the file
